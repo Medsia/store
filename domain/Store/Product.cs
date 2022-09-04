3de096b637f1c9509f29fa45c4cacee2d1997f00
Dmitry Guznenok <medsia@mail.ru>
2022-09-04 22:20:49 +0300

    Описана сущность Product

---
+++ domain/Store/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Store
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Product(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
