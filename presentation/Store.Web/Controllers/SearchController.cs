8efab3e343522ff744f342d8b3d62c4fa550f7a4
Dmitry Guznenok <medsia@mail.ru>
2022-09-05 00:52:20 +0300

    Добавлена страница поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public SearchController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Index(string query)
+        {
+            var products = productRepository.GetAllByTitle(query);
+
+            return View(products);
+        }
+    }
+}
