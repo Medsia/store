ab1eb3c5741baeccdbebd6a57ded21a93cf5312c
Dmitry Guznenok <medsia@mail.ru>
2022-09-07 17:04:00 +0300

    Добавлена страница содержимого корзины

---
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        private readonly IOrderRepository orderRepository;
+        public OrderController(IProductRepository productRepository,
+                              IOrderRepository orderRepository)
+        {
+            this.productRepository = productRepository;
+            this.orderRepository = orderRepository;
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.TryGetCart(out Cart cart))
+            {
+                var order = orderRepository.GetById(cart.OrderId);
+                OrderModel model = Map(order);
+
+                return View(model);
+
+            }
+
+            return View("Empty");
+        }
+
+        private OrderModel Map(Order order)
+        {
+            var productIds = order.Items.Select(item => item.ProductId);
+            var products = productRepository.GetAllByIds(productIds);
+            var itemModels = from item in order.Items
+                             join product in products on item.ProductId equals product.Id
+                             select new OrderItemModel
+                             {
+                                 ProductId = product.Id,
+                                 Title = product.Title,
+                                 Price = item.Price,
+                                 Count = item.Count,
+                             };
+
+            return new OrderModel
+            {
+                Id = order.Id,
+                Items = itemModels.ToArray(),
+                TotalCount = order.TotalCount,
+                TotalPrice = order.TotalPrice,
+
+            };
+        }
+        public IActionResult AddItem(int id)
+        {
+            
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
+
+            var product = productRepository.GetById(id);
+            order.AddItem(product, 1);
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+
+            HttpContext.Session.Set(cart);
+
+            return RedirectToAction("Index", "Product", new { id });
+        }
+    }
+}
